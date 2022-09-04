99e320d27fc9c6c07eb836659d6afda585714514
Maxim Petrusevich <maks1464@gmail.com>
2022-09-04 21:03:56 +0300

    Added quick sort implementation

--- src/main/java/com/megapapa/Application.java
+++ src/main/java/com/megapapa/Application.java
@@ -1,15 +1,20 @@
 package com.megapapa;
 
 import com.megapapa.sorts.MergeSort;
+import com.megapapa.sorts.QuickSort;
 import com.megapapa.tasks.SumOfTwoIntegersInArray;
 
+import java.util.ArrayList;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Application {
 
     public static void main(String[] args) {
 
-
+        var quickSort = new QuickSort();
+        IntStream.of(quickSort.sort(new int[]{45,12,85,32,89,39,69,44,42,1,6,8}))
+                .forEach(System.out::println);
 
 //        var sumOfTwo = new SumOfTwoIntegersInArray();
 //        var result = sumOfTwo.numberOfPairs(new int[]{0, 1, 3, 0}, 3);
--- src/main/java/com/megapapa/sorts/QuickSort.java
+++ src/main/java/com/megapapa/sorts/QuickSort.java
@@ -1,4 +1,47 @@
 package com.megapapa.sorts;
 
+import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class QuickSort {
+
+    public int[] sort(int[] values) {
+
+        if (values.length <= 1) {
+
+            return values;
+        }
+
+        int pivotElementIndex = ThreadLocalRandom.current().nextInt(0, values.length);;
+        int pivotElement = values[pivotElementIndex];
+        var lowerElems = new ArrayList<>();
+        var higherElems = new ArrayList<>();
+
+        for (int i = 0; i < values.length; i++) {
+
+            if (values[i] <= pivotElement) {
+
+                lowerElems.add(values[i]);
+            } else{
+
+                higherElems.add(values[i]);
+            }
+        }
+
+        var sortedLower = sort(lowerElems.stream().mapToInt(i -> (int) i).toArray());
+        var sortedHigher = sort(higherElems.stream().mapToInt(i -> (int) i).toArray());
+        int[] result = new int[sortedLower.length + sortedHigher.length];
+
+        for (int i = 0; i < sortedLower.length; i++) {
+
+            result[i] = sortedLower[i];
+        }
+
+        for (int i = 0; i < sortedHigher.length; i++) {
+
+            result[i + sortedLower.length] = sortedHigher[i];
+        }
+
+        return result;
+    }
 }
